d193ab82a30ffdd882d4f1150b4a65a669c647ed
LAYTHJABBAR <53024934+LAYTHJABBAR@users.noreply.github.com>
2020-04-05 03:53:23 -0600

    add a heroku server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-concentric-06337',
       user : '',
       password : '',
       database : 'test'
