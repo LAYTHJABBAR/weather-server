55e9e0f59b026771584011e919541342e479f51e
LAYTHJABBAR <53024934+LAYTHJABBAR@users.noreply.github.com>
2020-04-05 01:24:08 -0600

    initiate server

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const knex = require('knex');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : '',
+      password : '',
+      database : 'test'
+    }
+  });
+  
+const app = express();
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.get('/profile', (req, res) => {
+ db.select('*').from('data').then(data => {
+  res.send(data)
+})
+})
+
+
+app.post('/profile', (req,res) => {
+ const {name, temp, feellike, visibility, windspeed, status, date} = req.body
+ db('data').returning('*').insert({
+     name: name,
+     temp: temp,
+     feellike: feellike,
+     visibility: visibility,
+     windspeed: windspeed,
+     status: status,
+     date: date
+ }).then(response => {
+   res.json(response)
+ })
+ 
+})
+
+app.listen(3000)
